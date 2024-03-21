aa3583ee736fd4eddf92815c2460db695cbc49e4
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 02:48:43 +0300

    feat(reducer): create initial state, and product reducer

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,20 @@
+import { ActionTypes } from "../constants/actionTypes";
+
+const initialState = {
+    products:[{
+        id: 1,
+        title: "Trendify",
+        category: "Store",
+    }]
+}
+
+
+export const productReducer = (state, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+    
+        default:
+            return state;
+    }
+}
