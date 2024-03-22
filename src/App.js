e3c3ced57cf57df14416607acd145b8735a417d6
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 07:02:49 +0300

    feat(add); product list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import ProductListing from './components/ProductListing';
 
 function App() {
   return (
     <div className='App'>
     <Header />
+    <ProductListing />
     </div>
   );
 }
