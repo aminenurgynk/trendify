4860a3d142e344272739c6b5dc827396fb3197b8
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 03:57:09 +0300

    docs(export): changing export place

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { productReducer } from "./productReducer";
 
-export const reducers = ({
+const reducers = ({
     allProducts: productReducer,
 })
+
+export default reducers;
