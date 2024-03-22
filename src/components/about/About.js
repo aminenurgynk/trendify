9a8b236db6c8d203983deb32ea184e8951866471
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 22:43:09 +0300

    styles(fixed): about part

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,18 +1,26 @@
 // src/components/About.js
-import React from 'react';
-import { Typography, Container } from '@mui/material';
+import React from "react";
+import { Typography, Container, useTheme } from "@mui/material";
 
 const About = () => {
-    return (
-        <Container sx={{ my: 5 }}>
-            <Typography variant="h4" gutterBottom>
-                Hakkımızda
-            </Typography>
-            <Typography variant="body1">
-                Bu bölümde şirketiniz veya projeniz hakkında bilgiler yer alır. Misyonunuz, vizyonunuz ve hizmetleriniz hakkında ziyaretçilere bilgi verebilirsiniz.
-            </Typography>
-        </Container>
-    );
+
+    const theme = useTheme();
+
+  return (
+    <>
+      {/* AppBar komponentinin yüksekliğine eşit boşluk ekleyin */}
+      <div style={{ ...theme.mixins.toolbar }} />
+      <Container sx={{ my: 5 }}>
+        <Typography variant="h4" gutterBottom>
+          About
+        </Typography>
+        <Typography variant="body1">
+          This section contains information about your company or project. You can
+          provide information to visitors about your mission, vision and services.
+        </Typography>
+      </Container>
+    </>
+  );
 };
 
 export default About;
