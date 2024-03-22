5787799189048beec5e84243ea45489c052a8ada
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 20:42:01 +0300

    styles(header): try to do something

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,15 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import React from "react";
 
 const Header = () => {
-  const headerStyle = {
-    background: "#234",
-    minHeight: "50px",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: "calc(10px + 2vmin)",
-    color: "white",
-    padding: " 0",
-    margin: "0",
-    width: "100%",
-  };
   return (
-    <div style={headerStyle}>
-      <div>Trendify</div>
-    </div>
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit">
+          <div>Trendify</div>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
