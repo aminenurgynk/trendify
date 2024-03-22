473b69565807ba3c609d4c3fc6c4f96790ed0636
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 22:58:03 +0300

    style(list): list turn

--- src/components/product/ProductListing.js
+++ src/components/product/ProductListing.js
@@ -1,28 +1,30 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProducts } from '../../redux/actions/productAction';
-import ProductComponent from './ProductComponent';
-import { Grid } from '@mui/material';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../../redux/actions/productAction";
+import ProductComponent from "./ProductComponent";
+import { Grid, useTheme } from "@mui/material";
 
 const ProductListing = () => {
-
   const dispatch = useDispatch();
-  const products = useSelector((state) => state.allProducts.products); 
-
+  const products = useSelector((state) => state.allProducts.products);
+  const theme = useTheme();
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
   return (
-    <Grid container spacing={2}>
-      {products.map((product) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
-          <ProductComponent product={product} />
-        </Grid>
-      ))}
-    </Grid>
-  )
-}
+    <>
+      <div style={{ ...theme.mixins.toolbar }} />
+      <Grid container spacing={2} sx={{ mt: theme.spacing(2) }}>
+        {products.map((product) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
+            <ProductComponent product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
 
-export default ProductListing
+export default ProductListing;
