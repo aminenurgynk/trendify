1ff89bacda54d8e645badab3d124973b663f23b3
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 07:00:11 +0300

    styles(basic): basic css right in js file, ui will be come soon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
-    import React from 'react'
-    
-    const Header = () => {
-      return (
-        <div className='ui fixed menu'>
-            <div className='ui container center'>
-                Trendify
-            </div>
-        </div>
-      )
-    }
-    
-    export default Header
+import React from "react";
+
+const Header = () => {
+  const headerStyle = {
+    background: "#234",
+    minHeight: "50px",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white",
+    padding: " 0",
+    margin: "0",
+    width: "100%",
+  };
+  return (
+    <div style={headerStyle}>
+      <div>Trendify</div>
+    </div>
+  );
+};
+
+export default Header;
