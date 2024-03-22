d1d96b9e8071755c8896363d79288d629ae6c2d0
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 22:13:55 +0300

    feat(add): categories

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -2,12 +2,15 @@ import { ActionTypes } from "../constants/actionTypes";
 
 const intialState = {
   products: [],
+  categories: [],
 };
 
 export const productsReducer = (state = intialState, action) => {
   switch (action.type) {
     case ActionTypes.SET_PRODUCTS:
-        return { ...state, products: action.payload };
+      return { ...state, products: action.payload };
+    case ActionTypes.SET_CATEGORIES:
+      return { ...state, categories: action.payload };
     default:
       return state;
   }
@@ -17,7 +20,7 @@ export const selectedProductsReducer = (state = {}, action) => {
   console.log(action.type);
   switch (action.type) {
     case ActionTypes.SELECTED_PRODUCT:
-        return { ...state, ...action.payload };
+      return { ...state, ...action.payload };
     case ActionTypes.REMOVE_SELECTED_PRODUCT:
       return {};
     default:
