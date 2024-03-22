185795e08ed6f483fc336c750e612ab121e8ef16
Aminenur Göynük <amine.gynk@gmail.com>
2024-03-22 20:44:00 +0300

    style(pComponent): showing products

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
 
-const ProductComponent = () => {
+const ProductComponent = ({ product }) => {
   return (
-    <div>ProductComponent</div>
+    <Card>
+    <CardMedia
+      component="img"
+      height="140"
+      image={product.image}
+      alt={product.title}
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {product.title}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        ${product.price}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">View</Button>
+    </CardActions>
+  </Card>
   )
 }
 
